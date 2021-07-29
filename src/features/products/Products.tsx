67ffac55001aaafd88b16cb64172b83bcbbc9f1a
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2021-07-29 15:42:43 -0400

    add pagination

--- src/features/products/Products.tsx
+++ src/features/products/Products.tsx
@@ -1,29 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { AppRoutes } from "../../App";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { ProductCard } from "./ProductCard/ProductCard";
-import { loadProductsList, selectProducts } from "./productsSlice";
+import { loadProductsList, selectTotalProducts, selectProductsByPageNumber } from "./productsSlice";
+import Pagination from "rc-pagination";
+import { PRODUCTS_PER_PAGE } from "../../consts";
+
 
 export const Products: React.FC = () => {
-  const products = useAppSelector(selectProducts);
+  const [page, setPage] = useState<number>(1);
   const dispatch = useAppDispatch();
+  //Todo check multiple selector call
+  const products = useAppSelector((state) => selectProductsByPageNumber(state, page));
+  const totalProducts = useAppSelector(selectTotalProducts);
+
+  const pageChangeHandler = (page: number) => {
+    setPage(page);
+  }
 
   useEffect(() => {
     dispatch(loadProductsList());
   }, [dispatch]);
 
   return (
-    <ul>
-      {products.map((product) => {
-        return (
-          <li key={product.id}>
-            <Link to={`${AppRoutes.PRODUCTS}/${product.id}`}>
-              <ProductCard product={product} />
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <Pagination
+        onChange={pageChangeHandler}
+        current={page}
+        pageSize={PRODUCTS_PER_PAGE}
+        total={totalProducts}
+        hideOnSinglePage
+      />
+      <ul>
+        {products.map((product) => {
+          return (
+            <li key={product.id}>
+              <Link to={`${AppRoutes.PRODUCTS}/${product.id}`}>
+                <ProductCard product={product} />
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
--- src/features/products/productsSlice.ts
+++ src/features/products/productsSlice.ts
@@ -2,6 +2,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
 import { Config } from "../../config/config";
 import { Product, Status } from "../../app/types";
+import { PRODUCTS_PER_PAGE } from '../../consts';
 
 export const productsActionTypes = {
   LOAD_PRODUCTS_LIST: "products/LOAD_PRODUCTS_LIST",
@@ -47,5 +48,12 @@ export const productsSlice = createSlice({
 });
 
 export const selectProducts = (state: RootState) => state.products.products;
+
+export const selectProductsByPageNumber = (state: RootState, page: number) => {
+  const { products } = state.products;
+  return products.slice(PRODUCTS_PER_PAGE * page - PRODUCTS_PER_PAGE, PRODUCTS_PER_PAGE * page);
+}
+
+export const selectTotalProducts = (state: RootState) => state.products.products.length;
 
 export default productsSlice.reducer;
